2d9b188eafd4358de6ad9befa6373278b024d74e
RD0006 <radhikadn2022@gmail.com>
2026-07-08 10:04:03 +0530

    Updation

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ document.addEventListener("click", (e) => {
     }
 
     if (e.target.id == "login") {
+        
         const username = document.querySelector("#username").value;
         const password = document.querySelector("#password").value;
 
@@ -34,134 +35,14 @@ document.addEventListener("click", (e) => {
         lsd = JSON.parse(lsd);
 
         if (lsd[username] == password) {
-            const body = document.body;
-            body.innerHTML = `<main>
-        <div id="main-1">
-            <div id="main-1-part-1">
-                <div class="brandname-div">
-                    <i class="ri-stack-fill"></i>
-                    <div class="brandname">
-                        <h2>FinTrack Pro</h2>
-                        <p>Enterprise Finance</p>
-                    </div>
-                </div>
-                <div class="buttons-div">
-                    <button id="dashboard-button">
-                        <i class="ri-layout-grid-line"></i>
-                        Dashboard
-                    </button>
-                    <button id="settings-button">
-                        <i class="ri-settings-3-fill"></i>
-                        Settings
-                    </button>
-                </div>
-            </div>
-            <div id="main-1-part-2">
-                <button id="add-transaction">
-                    <i class="ri-add-line"></i>
-                    Add Transaction
-                </button>
-            </div>
-        </div>
-        <div id="main-2">
-            <div id="main-2-part-1">
-                <div id="username-display">
-                    Username
-                </div>
-                <button id="logout-button">
-                    Logout
-                </button>
-            </div>
-            <div id="main-2-part-2">
-                <div id="heading">
-                    <h1>Financial Overview</h1>
-                    <p>Real-time tracking application</p>
-                </div>
-                <div id="part-1">
-                    <div class="card" id="card-1">
-                        <div class="card-icon blue-card-icon">
-                            <i class="ri-bank-line"></i>
-                        </div>
-                        <p>Current Balance</p>
-                        <div class="card-value" id="current-balance">
-                            $0
-                        </div>
-                    </div>
-                    <div class="card" id="card-2">
-                        <div class="card-icon green-card-icon">
-                            <i class="ri-arrow-right-up-line"></i>
-                        </div>
-                        <p>Total Income</p>
-                        <div class="card-value" id="total-income">
-                            $0
-                        </div>
-                    </div>
-                    <div class="card" id="card-3">
-                        <div class="card-icon red-card-icon">
-                            <i class="ri-arrow-right-down-line"></i>
-                        </div>
-                        <p>Total Income</p>
-                        <div class="card-value" id="total-expense">
-                            $0
-                        </div>
-                    </div>
-                    <div class="card" id="card-4">
-                        <div class="card-icon blue-card-icon">
-                            <i class="ri-hand-coin-line"></i>
-                        </div>
-                        <p>Total Income</p>
-                        <div class="card-value" id="total-transactions">
-                            0
-                        </div>
-                    </div>
-                </div>
-                <div id="part-2">
-                    <h2>Cash Flow Analysis</h2>
-                    <canvas id="cashFlowChart" style="box-sizing: border-box; display: block; height: 250px; width: 536.9px;" width="805" height="375"></canvas>
-                </div>
-                <div id="part-3">
-                    <h2>Preferences</h2>
-                    <div id="theme-preference">
-                        <h4>Dark Mode</h4>
-                        <label class="switch">
-                            <input type="checkbox">
-                            <span class="slider round"></span>
-                        </label>
-                    </div>
-                    <button id="reset-all-data">
-                        <i class="ri-delete-bin-4-line"></i>
-                        Reset All Data
-                    </button>
-                </div>
-                <div id="part-4">
-                    <h2>All Transactions</h2>
-                    <table>
-                        <th>
-                            DATE
-                        </th>
-                        <th>
-                            DESCRIPTION
-                        </th>
-                        <th>
-                            CATEGORY
-                        </th>
-                        <th>
-                            AMOUNT
-                        </th>
-                        <th>
-                            ACTIONS
-                        </th>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </main>`;
+            const main = document.querySelector("main");
+            const loginDiv = document.querySelector("#login-div");
+            loginDiv.style.display = "none";
+            main.style.display = "flex";
         }
         else {
             window.alert("Invalid username or password!");
         }
-
-        
     }
 
     if (e.target.id == "register") {
@@ -197,6 +78,61 @@ document.addEventListener("click", (e) => {
             const accountText = document.querySelector("#account-text");
             accountText.innerHTML = "Don't have an account? <span id=\"register-here\">Register here</span>";
         }
+    }
+
+    if (e.target.id == "add-transaction") {
+        const dialog = document.querySelector("#add-transaction-dialog");
+        const form = document.querySelector("#add-transaction-form");
+        dialog.showModal();
+        dialog.style.display = "flex";
+    }
+
+    if (e.target.id == "save-transaction") {
+        e.preventDefault();
+
+        const dialog = document.querySelector("#add-transaction-dialog");
+        const form = document.querySelector("#add-transaction-form");
+
+        const transactionType = document.querySelector("#transaction-type").value;
+        const transactionDescription = document.querySelector("#transaction-description").value;
+        const transactionAmount = document.querySelector("#transaction-amount").value;
+        const transactionDate = document.querySelector("#transaction-date").value;
+        const transactionCategory = document.querySelector("#transaction-category").value;
+
+        if (transactionType.trim() == "" || transactionDescription.trim() == "" || transactionAmount.trim() == "" || transactionDate.trim() == "" || transactionCategory.trim() == "") 
+        {
+            window.alert("Please fill all fields");
+            return;
+        }
+        console.log(transactionType);
+        console.log(transactionDescription);
+        console.log(transactionAmount);
+        console.log(transactionDate);
+        console.log(transactionCategory);
+
+        const table = document.querySelector("table");
+        table.innerHTML += `
+            <tr>
+                <td>
+                    ${transactionDate}
+                </td>
+                <td>
+                    ${transactionDescription}
+                </td>
+                <td>
+                    ${transactionCategory}
+                </td>
+                <td>
+                    ${transactionAmount}
+                </td>
+                <td>
+                    
+                </td>
+            </tr>
+        `
+        dialog.close();      
+        dialog.style.display = "none";
+        form.reset();
     }
 });
 
